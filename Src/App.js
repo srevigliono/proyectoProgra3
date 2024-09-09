ae1d512b67d1da1c12b0c80139a8d3e5de017f7f
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-09-09 18:11:54 -0300

    header y footer terminados

--- Src/App.js
+++ Src/App.js
@@ -11,7 +11,9 @@ function App() {
         <Header/>
     </header>
     
-    
+    <body>
+    </body>
+
 
     <footer>
         <Footer/>
--- Src/Components/Footer/Footer.js
+++ Src/Components/Footer/Footer.js
@@ -1,9 +1,24 @@
 import "./Footer.css"
 
+
+
 const Footer = () => {
     return(
         <footer>
-            <p>Valentino Miotto, Santiago Revigliono and Santiago Legarre</p>
+            <div> 
+                <i className="fas fa-user"></i><span>Valentino Miotto</span>
+                <i className="fas fa-user"></i><span>Santiago Revigliono</span>
+                <i className="fas fa-user"></i><span>Santiago Legarre</span>
+            </div>
+            <div>
+                <p></p>
+            </div>
+            <div class="iconos">
+                <i className="fab fa-github"></i>
+                <i className="fab fa-instagram"></i>
+                <i className="fab fa-twitter"></i>
+            </div>
+
         </footer>
     )
 }
