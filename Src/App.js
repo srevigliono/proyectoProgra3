74e067d4bb4f36d4dd6e64e039bdbf73cfcf92a1
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-09-10 16:53:26 -0300

    empezando la home page

--- Src/App.js
+++ Src/App.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header/Header"
 import Footer from "./components/Footer/Footer"
+import Home from './components/Home/Home'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     </header>
     
     <body>
+      <Home/>
     </body>
 
 
