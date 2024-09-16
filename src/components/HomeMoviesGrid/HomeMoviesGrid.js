1aa706d7a2624d459a3563517c286b316c3173b8
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-09-16 17:31:29 -0300

    pagina home terminada

--- src/components/HomeMoviesGrid/HomeMoviesGrid.js
+++ src/components/HomeMoviesGrid/HomeMoviesGrid.js
@@ -12,7 +12,7 @@ class HomeMoviesGrid extends Component {
   }
 
   componentDidMount() {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1')
+    fetch(this.props.url)
       .then(response => response.json())
       .then(data => {
         if (data && data.results) { 
@@ -26,27 +26,14 @@ class HomeMoviesGrid extends Component {
 
   render() {
     const { peliculas } = this.state;
-
-    const peliculasPopulares = peliculas.slice(0, 5);
-    const peliculasCartelera = peliculas.slice(5, 10); 
+    const movies = peliculas.slice(0, 5);
 
     return (
       <section className="seccion">
-        <h2 className="populares-titulo">Películas Populares</h2>
         <h3 className="vermas"><Link to={"/all-movies"}>Ver todas</Link></h3>
         <div className="populares-grid-container">
-          {peliculasPopulares.length > 0
-            ? peliculasPopulares.map((movie, index) => (
-                <HomeMovies movie={movie} key={index} />
-              ))
-            : <p className="cargando">Cargando...</p>}
-        </div>
-
-        <h2 className="populares-titulo">Películas en Cartelera</h2>
-        <h3 className="vermas"><Link to={"/all-movies"}>Ver todas</Link></h3>
-        <div className="container">
-          {peliculasCartelera.length > 0
-            ? peliculasCartelera.map((movie, index) => (
+          {movies.length > 0
+            ? movies.map((movie, index) => (
                 <HomeMovies movie={movie} key={index} />
               ))
             : <p className="cargando">Cargando...</p>}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -13,8 +13,10 @@ class Home extends Component {
           <header>
           </header>
           <SearchForm history={this.props.history} />
-          <HomeMoviesGrid url={'https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1'} />
-          <HomeMoviesGrid url={'https://api.themoviedb.org/3/movie/popular?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1'} />
+          <h2 className="titulo">Películas Cartelera</h2>
+          <HomeMoviesGrid url={'https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1'}/>
+         <h2 className="titulo">Películas Populares</h2>
+          <HomeMoviesGrid url={'https://api.themoviedb.org/3/movie/popular?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1'}/>
         </main>
       </>
     );
