023f41f093f367ac16b452a9c27aab93401db005
santiagolegarre <slegarrematera@udesa.edu.ar>
2024-09-17 14:11:47 -0300

    Error404 arreglo

--- src/components/Error404/Error404.js
+++ src/components/Error404/Error404.js
@@ -5,10 +5,9 @@ class Error404 extends Component {
     render() {
       return (
         <section className="text-error">
-        <img src="/img/Error404.jpg" alt="Error 404" className="imagen-error" />
         <p>Error:</p>
-        <p>La pagina no existe pipi</p>
-        <p>Haga click <a href='/' className="link-error">AQUI</a> para volver a la página de inicio
+        <p>La pagina que estas buscando no existe</p>
+        <p>Haga click <a href='/' className="link-error">Aca!</a> para volver a la página de inicio
         </p>
     </section>
         )
