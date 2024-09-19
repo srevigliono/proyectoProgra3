e43ae2a2e0066dbb0428eb721ffdb715b7e3d9e7
santiagolegarre <slegarrematera@udesa.edu.ar>
2024-09-19 16:47:59 -0300

    boton ver mas populares, pero no se muestran. Falta arreglar

--- src/components/PopularesGrid/PopularesGrid.js
+++ src/components/PopularesGrid/PopularesGrid.js
@@ -3,42 +3,54 @@ import PeliculasPopulares from "../PeliculasPopulares/PeliculasPopulares";
 import './PopularesGrid.css'
 
 class PopularesGrid extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      peliculas: []
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            peliculas: [],
+            cargarMasPeliculas: false
 
-  componentDidMount() {
-    fetch('https://api.themoviedb.org/3/movie/popular?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1')
-      .then(response => response.json())
-      .then(data => {
-        if (data && data.results) {
-          this.setState({ peliculas: data.results });
-        } else {
-          console.error("No se encuentran películas");
-        }
-      })
-      .catch(error => console.log(error));
-  }
+        };
+    }
 
-  render() {
-    const { peliculas } = this.state;
+    componentDidMount() {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1')
+            .then(response => response.json())
+            .then(data => {
+                if (data && data.results) {
+                    this.setState({ peliculas: data.results.slice(0, 10) });
+                } else {
+                    console.error("No se encuentran películas");
+                }
+            })
+            .catch(error => console.log(error));
+    }
 
-    return (
-      <section className="seccion">
-        <h2 className="titulo">Todas las Peliculas más Populares</h2>
-        <div className="peliculas-grid-container">
-          {peliculas.length > 0
-            ? peliculas.map((movie, index) => (
-                <PeliculasPopulares movie={movie} key={index} />
-              ))
-            : <p className="cargando">Cargando...</p>} 
-        </div>
-      </section>
-    );
-  }
+
+
+    cargarMasPeliculas() {
+        this.setState({
+            cargarMasPeliculas: true
+        })
+    }
+
+    render() {
+        const { peliculas } = this.state;
+
+        return (
+            <section className="seccion">
+                <h2 className="titulo">Todas las Peliculas más Populares</h2>
+                <div className="peliculas-grid-container">
+                    {peliculas.length > 0
+                        ? peliculas.map((movie, index) => (
+                            <PeliculasPopulares movie={movie} key={index} />
+                        ))
+                        : <p className="cargando">Cargando...</p>}
+                </div>
+                <button className={this.state.cargarMasPeliculas ? 'cargarMasPeliculas': 'cargarMasPeliculas'} onClick={() => this.cargarMasPeliculas()} >  
+                    {this.state.cargarMasPeliculas ? 'Mostrar Menos Peliculas Populares' : 'Mostrar Mas Peliculas Populares'} </button>
+            </section>
+        );
+    }
 }
 
 export default PopularesGrid;
