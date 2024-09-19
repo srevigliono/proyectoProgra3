478701ab50da2cc1827d60b4abf4559c7edff9b4
santiagolegarre <slegarrematera@udesa.edu.ar>
2024-09-19 16:28:41 -0300

    ver todas Populares

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Error404 from "./components/Error404/Error404";
 import { Switch, Route } from 'react-router-dom';
 import { SearchResults } from "./pages/SearchResults";
 import Cartelera from "./pages/Cartelera";
+import Populares from "./pages/Populares";
 
 
 
@@ -22,6 +23,7 @@ function App() {
             <Route path='/' exact={true} component={Home} ></Route>
             <Route path="/search" component={SearchResults}></Route>
             <Route path="/peliculas-cartelera" component={Cartelera} />
+            <Route path="/peliculas-populares" component={Populares} />
             <Route path="" component={Error404} />
           </Switch>
 
---
+++ src/pages/Populares.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import PopularesGrid from "../components/PopularesGrid/PopularesGrid";
+
+class Populares extends Component {
+
+  render(
+
+    ) {
+      return (
+        <>
+          <main>
+  
+            <header>
+            </header>
+            <PopularesGrid/>
+          </main>
+        </>
+      );
+    }
+}
+
+export default Populares;
