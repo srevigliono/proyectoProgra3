70ba04b674faedd976305232ee87ecf9cc3754fe
Valentino Miotto <vmiotto@udesa.edu.ar>
2024-09-23 14:53:55 -0300

    cambie el filtrado de peliculas con un input para que a medida que vayas escribiendo vaya filtrando

--- src/components/CarteleraGrid/CarteleraGrid.js
+++ src/components/CarteleraGrid/CarteleraGrid.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import PeliculasCartelera from "../PeliculasCartelera/PeliculasCartelera";
-
 import './CarteleraGrid.css'
 
 class CarteleraGrid extends Component {
@@ -8,22 +7,12 @@ class CarteleraGrid extends Component {
     super(props);
     this.state = {
       peliculas: [],
-      generos: [],
-      selectedGenero: ""
+      search: ""
     };
   }
 
   componentDidMount() {
-    this.fetchPeliculas();
-    this.fetchGeneros();
-  }
-
-  fetchPeliculas = (generoId = "") => {
-    const url = generoId
-      ? `https://api.themoviedb.org/3/discover/movie?api_key=e6a0d8ba2d9778d0953077400f26f011&with_genres=${generoId}`
-      : `https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1`;
-    
-    fetch(url)
+    fetch('https://api.themoviedb.org/3/movie/now_playing?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US&page=1')
       .then(response => response.json())
       .then(data => {
         if (data && data.results) {
@@ -35,48 +24,34 @@ class CarteleraGrid extends Component {
       .catch(error => console.log(error));
   }
 
-  fetchGeneros = () => {
-    fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=e6a0d8ba2d9778d0953077400f26f011&language=en-US')
-      .then(response => response.json())
-      .then(data => {
-        if (data && data.genres) {
-          this.setState({ generos: data.genres });
-        }
-      })
-      .catch(error => console.log(error));
-  }
-
-  CambioGenero = (event) => {
-    const selectedGenero = event.target.value;
-    this.setState({ selectedGenero });
-    this.fetchPeliculas(selectedGenero); 
-  }
+  handleSearch = (event) => {
+    this.setState({ search: event.target.value });
+  };
 
   render() {
-    const { peliculas, generos, selectedGenero } = this.state;
+    const { peliculas, search } = this.state;
+
+    const peliculasFiltradas = peliculas.filter((movie) =>
+      movie.title.toLowerCase().includes(search.toLowerCase())
+    );
 
     return (
       <section className="cartelera-seccion">
         <h2 className="titulo">Todas las Películas en Cartelera</h2>
-
-       
-        <form className="formulario-filtrar">
-          <select id="genero" value={selectedGenero} onChange={this.CambioGenero}>
-            <option value="">Todos los Géneros</option>
-            {generos.map((genero) => (
-              <option key={genero.id} value={genero.id}>
-                {genero.name}
-              </option>
-            ))}
-          </select>
-        </form>
+        
+        <input
+          type="text" className="search-input"
+          placeholder="Buscar por título..."
+          value={search}
+          onChange={this.handleSearch}
+        />
 
         <div className="peliculas-grid-container">
-          {peliculas.length > 0
-            ? peliculas.map((movie, index) => (
+          {peliculasFiltradas.length > 0
+            ? peliculasFiltradas.map((movie, index) => (
                 <PeliculasCartelera movie={movie} key={index} />
               ))
-            : <p className="cargando">Cargando...</p>}
+            : <p className="cargando">No se encontraron películas</p>}
         </div>
       </section>
     );
