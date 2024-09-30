13c31bea19c51368996ca68e733487f4019953ed
Santiago Revigliono <srevigliono@udesa.edu.ar>
2024-09-30 11:29:13 -0300

    Css favoritos y arreglo del promise de la función agregar a favoritos

--- src/pages/Favoritos.js
+++ src/pages/Favoritos.js
@@ -13,39 +13,55 @@ class Favoritos extends Component {
 
     componentDidMount() {
         const storage = localStorage.getItem("favoritos");
-        const parsedArray = JSON.parse(storage);
-
-        Promise.all(
-            parsedArray.map((id) => {
-                return fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0ac8f3235ecd7f1b9c2f99fa8b233126`)
-                    .then((response) => response.json())
-                    .then((movie) => 
-                        this.setState({
-                            movies: [...this.state.movies, movie],
-                        })
-                    )
-                    .catch((error) => {
-                        console.error("Error fetching movie:", error);
+        if (storage !== null) {
+            const parsedArray = JSON.parse(storage);
+            Promise.all(
+                parsedArray.map((id) =>
+                    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=0ac8f3235ecd7f1b9c2f99fa8b233126`)
+                        .then((response) => response.json())
+                )
+            )
+                .then((data) => {
+                    this.setState({
+                        movies: data,
+                        isLoading: false,
                     });
-            })
-        ).then(() => {
+                })
+                .catch((error) => {
+                    console.error("Error fetching movie data:", error);
+                    this.setState({
+                        isLoading: false,
+                    });
+                });
+        } else {
             this.setState({
                 isLoading: false,
             });
-        });
+        }
     }
 
     render() {
         return (
-            <>
-                <div>
-                    {this.state.movies.map((unaFav, idx) => (
-                        <HomeMovies key={unaFav.name + idx} movie={unaFav} />
-                    ))}
+            <div className="seccion">
+                <h2 className="titulo">Tus Películas Favoritas</h2>
+                <div className="populares-grid-container">
+                    {this.state.isLoading ? (
+                        <p className="cargando">Cargando...</p>
+                    ) : (
+                        this.state.movies.map((unaFav, idx) => (
+                            <HomeMovies key={unaFav.id + idx} movie={unaFav} className="pelicula" />
+                        ))
+                    )}
                 </div>
-            </>
+                {this.state.movies.length === 0 ? (
+                    <p className="cargando">No tienes películas favoritas guardadas.</p>
+                ) : null}
+                <div className="vermas">
+                    <a href="/">Volver a la página principal</a>
+                </div>
+            </div>
         );
     }
 }
 
-export default Favoritos;
+export default Favoritos
